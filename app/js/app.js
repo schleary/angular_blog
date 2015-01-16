63eafb23a19fad9f13223671ce1ab6c20e2aabe9
Holly Leary <schleary@gmail.com>
2015-01-16 11:24:39 -0800

    new posts

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,33 @@
 var blahgApp = angular.module('blahgApp', [
   'ui.router',
-  'homeControllerModule'
+  'homeControllerModule',
+  'postsControllerModule'
 ]);
 
 blahgApp.config(function($stateProvider, $urlRouterProvider) {
+    // routing area
     $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '../views/home.html',
-    })
-  $urlRouterProvider.otherwise('/home');
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/views/home.html',
+      })
+      .state('posts', {
+        url: '/posts',
+        templateUrl: 'app/views/posts.html'
+      })
+      .state('posts.new', {
+        url: '/new-post',
+        views: {
+          'new': {
+            templateUrl: 'app/views/new.html',
+          }
+        }
+      })
+      .state('show', {
+        url: '/post/:id',
+        templateUrl: 'app/views/show.html',
+
+      })
+
+  $urlRouterProvider.otherwise('/');
 });
